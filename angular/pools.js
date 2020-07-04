cf37a53fb7fc80df3bec9792c022d0206e15e172
4cos90 <910508524@qq.com>
2020-07-05 02:18:42 +0800

    first commit

---
+++ angular/pools.js
@@ -0,0 +1,70 @@
+(function(global, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = factory();
+    } else {
+        global.Pools = factory();
+    }
+})(this, function() {
+
+    function Pools() {
+        var __ChanList = {};
+        var __FnList = {};
+
+        this.NewChannel = function(key, fn) {
+            if (typeof __FnList[key] === 'undefined') {
+                __FnList[key] = [fn];
+            } else {
+                __FnList[key].push(fn);
+            }
+        };
+
+        this.Receive = function(key, fnkey, obj) {
+            if (!__FnList[fnkey]) {
+                return;
+            }
+            if (typeof __ChanList[key] === 'undefined') {
+                __ChanList[key] = [{ fnkey: fnkey, obj: obj }];
+            } else {
+                __ChanList[key].push({ fnkey: fnkey, obj: obj });
+            }
+        };
+
+        this.Send = function(key, rlt) {
+            if (!__ChanList[key] || __ChanList[key].length == 0) {
+                return;
+            }
+            var fnkey = __ChanList[key][0].fnkey;
+            if (!__FnList[fnkey]) {
+                return;
+            }
+            if (__FnList[fnkey] instanceof Array && __ChanList[key] instanceof Array && __ChanList[key].length > 0) {
+                for (var i = __FnList[fnkey].length - 1; i >= 0; i--) {
+                    __FnList[fnkey][i].call(__ChanList[key][0].obj, rlt);
+                }
+
+            }
+        };
+
+        this.Finish = function(key) {
+            if (__ChanList[key] && __ChanList[key].length >= 1) {
+                __ChanList[key].splice(0, 1);
+            }
+        };
+
+        this.ClearChannel = function(key) {
+            for (key in __ChanList) {
+                if (__ChanList[key] instanceof Array && __ChanList[key].length > 0) {
+                    __ChanList[key] = __ChanList[key].filter(o => o.fnkey != key);
+                }
+            }
+        };
+
+        this.ClearReceive = function(key) {
+            __ChanList[key] = [];
+        };
+    }
+
+    return Pools;
+});
